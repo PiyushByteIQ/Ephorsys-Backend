7c595ec86c1572f54f3d59d36545ba6bb6000b83
Piyush Hazarii <piyush.byteiq@gmail.com>
2025-11-01 18:44:27 +0530

    Ephorsys

--- src/main/java/company/ephorsys/cofig/CorsConfig.java
+++ src/main/java/company/ephorsys/cofig/CorsConfig.java
@@ -15,10 +15,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("*") // Adjust this to your React app's URL
                         .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
